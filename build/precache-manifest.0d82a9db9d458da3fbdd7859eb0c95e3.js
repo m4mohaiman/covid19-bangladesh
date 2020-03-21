5c627db738ea3b5021187e94c9c989515fc79b7b
mohaiman.dev <mohaiman.dev@gmail.com>
2020-03-21 17:14:30 +0600

    first commit

---
+++ build/precache-manifest.0d82a9db9d458da3fbdd7859eb0c95e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "981900ba6074c72c64d1b66e2babfbb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00d101aa1ea0319ff1e0",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "4e9193cc6df5b671c031",
+    "url": "/static/css/main.fef3e611.chunk.css"
+  },
+  {
+    "revision": "00d101aa1ea0319ff1e0",
+    "url": "/static/js/2.7b89e658.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7b89e658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e9193cc6df5b671c031",
+    "url": "/static/js/main.08dfe7a7.chunk.js"
+  },
+  {
+    "revision": "2d8f72aeca456b536118",
+    "url": "/static/js/runtime-main.c2717fbf.js"
+  }
+]);
